e446b24b72da9370959ac9ce5c501a1f10d08f3f
Aric Volman <89823522+aric-volman@users.noreply.github.com>
2024-12-01 22:41:38 -0800

    Add Encoder ino
    Uses PIO and encoder library in order to use encoders

---
+++ sketch_dec1b.ino
@@ -0,0 +1,24 @@
+#include "pio_encoder.h"
+
+// List of encoder pins that work
+// 12
+// 10
+// 8
+PioEncoder encoder(10);
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  encoder.begin();
+Serial.begin();
+    pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  delay(20);
+  
+  Serial.println(encoder.getCount());
+
+}
